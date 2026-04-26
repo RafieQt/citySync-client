c6a1b090c272009a80acd12871e16803fa983b24
RafieQt <tajuddinrafi76@gmail.com>
2026-04-26 23:46:32 +0600

    home button added

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -2,6 +2,7 @@ import Lottie from 'lottie-react';
 import errorAnimation from '../../assets/animation/warning.json';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import { Link } from 'react-router';
 
 const LottieComponent = Lottie?.default || Lottie;
 
@@ -9,14 +10,19 @@ const ErrorPage = () => {
     return (
         <div className=''>
             <Navbar></Navbar>
-            <div className='flex items-center justify-center min-h-screen'>
-                <div className='mx-auto' style={{ width: 300 }}>
-                    <LottieComponent
-                        animationData={errorAnimation}
-                        loop={true}
-                        style={{ width: '100%', height: '100%' }}
-                    />
+            <div className='flex flex-col gap-4 items-center min-h-screen'>
+                <div className='flex items-center justify-center my-20'>
+                    <div className='mx-auto' style={{ width: 300 }}>
+                        <LottieComponent
+                            animationData={errorAnimation}
+                            loop={true}
+                            style={{ width: '100%', height: '100%' }}
+                        />
+                    </div>
                 </div>
+                <Link to='/'>
+                    <button className='btn bg-[#CDEEEE] text-[#03373D] mx-auto text-center w-20 font-bold'>Home</button>
+                </Link>
             </div>
             <Footer></Footer>
         </div>
