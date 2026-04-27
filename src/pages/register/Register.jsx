a7c488314977f87420cf53baec456c10b23f3d72
RafieQt <tajuddinrafi76@gmail.com>
2026-04-27 20:30:36 +0600

    form validation for reigster and login done

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,25 +1,47 @@
 import { Link } from 'react-router';
 import signin from '../../assets/animation/signin.png'
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleRegister = (data) => {
+        console.log(data);
+    }
     return (
         <div className="flex gap-3 items-center justify-between bg-gradient-to-r from-[#E0F7F5] to-[#CDEEEE] w-full rounded-lg p-5 px-20 mb-2 mt-10">
             <div>
-                <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-[450px] border p-4">
+                <form onSubmit={handleSubmit(handleRegister)}>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-[450px] border p-4">
 
+                        {/* Name */}
+                        <label className="label">Name</label>
+                        <input {...register("name", { required: true })} type="text" className="input w-full p-2" placeholder="Your Name" />
+                        {
+                            errors.name?.type === "required" && <p>Name is Required!</p>
+                        }
 
-                    <label class="label">Name</label>
-                    <input type="text" class="input w-full p-2" placeholder="Your Name" />
+                        {/* Email */}
+                        <label className="label">Email</label>
+                        <input {...register("email", { required: true })} type="email" className="input w-full p-2" placeholder="Your Email" />
+                        {
+                            errors.email?.type === "required" && <p>Email is Required!</p>
+                        }
 
-                    <label class="label">Email</label>
-                    <input type="email" class="input w-full p-2" placeholder="Your Email" />
+                        {/* Password */}
+                        <label className="label">Password</label>
+                        <input {...register("password", { required: true, minLength: 6 })} type="password" className="input w-full p-2" placeholder="At least six characters or digit." />
+                        {
+                            errors.password?.type === "required" && <p>Password is Required!</p>
+                        }
+                        {
+                            errors.password?.type === "minLength" && <p>Minimum Length is 6!</p>
+                        }
 
-                    <label class="label">Password</label>
-                    <input type="password" class="input w-full p-2" placeholder="At least six characters or digit." />
-
-                    <button class="btn mt-4 w-full bg-[#03373D] text-white font-semibold">Login</button>
-                    <p className='text-center'>Already a User? <Link to='/signin' className='text-[#03373D]  hover:underline font-bold'>Sign In</Link></p>
-                </fieldset>
+                        <button className="btn mt-4 w-full bg-[#03373D] text-white font-semibold">Login</button>
+                        <p className='text-center'>Already a User? <Link to='/signin' className='text-[#03373D]  hover:underline font-bold'>Sign In</Link></p>
+                    </fieldset>
+                </form>
             </div>
             <div className="w-[400px]">
                 <img src={signin} alt="" />
--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,22 +1,42 @@
 import { Link } from 'react-router';
 import signin from '../../assets/animation/signin.png'
+import { useForm } from 'react-hook-form';
 
 const SignIn = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleLogin = (data) => {
+        console.log(data);
+    }
+
     return (
         <div className="flex gap-3 items-center justify-between bg-gradient-to-r from-[#E0F7F5] to-[#CDEEEE] w-full rounded-lg p-5 px-20 mb-2 mt-10">
             <div>
-                <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-[450px] border p-4">
-                    
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-[450px] border p-4">
 
-                    <label class="label">Email</label>
-                    <input type="email" class="input w-full  p-2" placeholder="Your Email" />
 
-                    <label class="label">Password</label>
-                    <input type="password" class="input w-full p-2" placeholder="Your Password" />
+                        <label className="label">Email</label>
+                        <input {...register("email", { required: true })} type="email" className="input w-full  p-2" placeholder="Your Email" />
+                        {
+                            errors.email?.type === "required" && <p>Email is required!</p>
+                        }
 
-                    <button class="btn mt-4 w-full bg-[#03373D] text-white font-semibold">Login</button>
-                    <p className='text-center'>New User? <Link to='/register' className='text-[#03373D]  hover:underline font-bold'>Register</Link></p>
-                </fieldset>
+                        <label className="label">Password</label>
+                        <input {...register("password", { required: true, minLength: 6 })} type="password" className="input w-full p-2" placeholder="Your Password" />
+                        {
+                            errors.password?.type === "required" && <p>Password is required!</p>
+                        }
+                        {
+                            errors.password?.type === "minLength" && <p>Password Length is Short!</p>
+                        }
+
+
+                        <button className="btn mt-4 w-full bg-[#03373D] text-white font-semibold">Login</button>
+                        <p className='text-center'>New User? <Link to='/register' className='text-[#03373D]  hover:underline font-bold'>Register</Link></p>
+                    </fieldset>
+                </form>
             </div>
             <div className="w-[400px]">
                 <img src={signin} alt="" />
