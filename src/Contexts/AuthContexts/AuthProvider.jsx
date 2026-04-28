f2ad186b66ae4a2873510b96c5d857fe7ae350bd
RafieQt <tajuddinrafi76@gmail.com>
2026-04-28 23:27:28 +0600

    hook created to get auth related functions easily

--- src/Contexts/AuthContexts/AuthProvider.jsx
+++ src/Contexts/AuthContexts/AuthProvider.jsx
@@ -1,10 +1,20 @@
 
 import { AuthContext } from './AuthContext';
-
+import {  createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../../firebase/firebase.init'
 const AuthProvider = ({children}) => {
 
-    const authInfo = {
+    const registerUser = (email, password)=>{
+        return createUserWithEmailAndPassword(email, password, auth)
+    }
 
+    const signUser = (email, password)=>{
+        return signInWithEmailAndPassword( email, password)
+    }
+
+    const authInfo = {
+        registerUser,
+        signUser
     }
 
     return (
