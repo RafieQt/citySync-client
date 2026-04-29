9e91fc310da43e7030810770012954dde4d819a0
RafieQt <tajuddinrafi76@gmail.com>
2026-04-29 11:47:18 +0600

    navbar login and logout functionality added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router";
+import useAuth from "../../hooks/useAuth";
 import Logo from "../ErrorPage/logo/Logo";
 
 
 const Navbar = () => {
+    const { user, logout } = useAuth();
+
+    const handleLogout = () => {
+        logout();
+    }
     return (
         <div className="max-w-7xl flex flex-col pt-5 mx-auto">
             <div className="navbar bg-[#EBFFFD] shadow-sm rounded-lg px-6">
@@ -9,27 +16,33 @@ const Navbar = () => {
                     <Logo></Logo>
                 </div>
                 <div className="flex-none">
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img
-                                    alt="Tailwind CSS Navbar component"
-                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    {
+                        user ? <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img
+                                        alt="Tailwind CSS Navbar component"
+                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                </div>
                             </div>
-                        </div>
-                        <ul
-                            tabIndex="-1"
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li>
-                                <a className="justify-between">
-                                    User Name.
-                                    <span className="badge">Premium</span>
-                                </a>
-                            </li>
-                            <li><a>Dashboard</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
-                    </div>
+                            <ul
+                                tabIndex="-1"
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                <li>
+                                    <a className="justify-between">
+                                        User Name.
+                                        <span className="badge">Premium</span>
+                                    </a>
+                                </li>
+                                <li><a>Dashboard</a></li>
+                                <button onClick={handleLogout} className="btn">Logout</button>
+                            </ul>
+                        </div> : <Link to='/signin'>
+                            <button className="btn bg-[#03373D] text-white font-semibold px-3">Login</button>
+                        </Link>
+                    }
+
+
                 </div>
             </div>
         </div>
