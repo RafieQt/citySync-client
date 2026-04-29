ca7189f176d42422ca53bbea95c6bd63629b7028
RafieQt <tajuddinrafi76@gmail.com>
2026-04-29 11:38:10 +0600

    enhance AuthProvider to manage user state and loading status with logout functionality

--- src/Contexts/AuthContexts/AuthProvider.jsx
+++ src/Contexts/AuthContexts/AuthProvider.jsx
@@ -1,29 +1,53 @@
 
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from '../../firebase/firebase.init'
+import { useEffect, useState } from 'react';
 
 
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
 
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     const registerUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const signUser = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     const signInGoogle = ()=>{
+        setLoading(true);
         return signInWithPopup(auth, googleProvider);
     }
 
+    const logout= ()=>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser);
+        })
+        return ()=>{
+            unsubscribe();
+        }
+    }, [])
+
     const authInfo = {
         registerUser,
+        loading,
+        user,
         signUser,
-        signInGoogle
+        signInGoogle,
+        logout
     }
 
     return (
